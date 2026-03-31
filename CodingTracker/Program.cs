9db37603d41cd2caec8108a704782fc5506301c5
eryccruz <eryccruz@gmail.com>
2026-03-31 13:19:05 +0800

    Update Program.cs
    keeping one message

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C# Academy! One More modification");
-Console.WriteLine("Hello, C# Academy! One More modification");
-Console.WriteLine("Hello, C# Academy! One More modification");
-Console.WriteLine("Hello, C# Academy! One More modification");
-Console.WriteLine("Hello, C# Academy! One More modification");
-Console.WriteLine("Hello, C# Academy! One More modification");
